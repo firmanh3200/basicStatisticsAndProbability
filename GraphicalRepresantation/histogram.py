a6413be2866dc4e1fcff65373ab12d73ab8ccb80
K.M Shariat Ullah <cast.shariat@gmail.com>
2022-09-20 23:14:44 +0600

    data meaning clearance

--- GraphicalRepresantation/histogram.py
+++ GraphicalRepresantation/histogram.py
@@ -2,6 +2,7 @@ import matplotlib.pyplot as plt
 
 #histogram
 
+#these are the marks of students in the course Numerical Analysis
 data = [100, 58,41,50,65,75,52,55,43,60,66,46,30,50,100,66,60,41,71,100,62,66,71,52,54,89,77,60,66,65,77,55,40,43,40,77,65,41,43,43,56,43,41,73,41,50,40,40,43,60,41,41]
 
 plt.xlabel('Marks')

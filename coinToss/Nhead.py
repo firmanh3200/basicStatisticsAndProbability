fcaab87bb9416933ca0742e9c25e38627b7d95b0
K.M Shariat Ullah <cast.shariat@gmail.com>
2023-02-13 23:34:13 +0600

    comments to the code

--- coinToss/Nhead.py
+++ coinToss/Nhead.py
@@ -2,9 +2,9 @@
 
 import random
 
-N = 200
-sampleSpace = ['H','T']
-count = 0
+N = 200 #number of trials
+sampleSpace = ['H','T'] 
+count = 0 #no of heads
 
 for i in range(N):
     if random.choices(sampleSpace)==['H']:

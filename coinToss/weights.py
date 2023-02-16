243d2e7316adaaddb189f925c51362febaabc85e
K.M Shariat Ullah <cast.shariat@gmail.com>
2023-02-16 14:20:43 +0600

    updated the weights

--- coinToss/weights.py
+++ coinToss/weights.py
@@ -1,5 +1,5 @@
 """
-Now, think about a coin for which the probability of a head is 0.1%. You toss the coin Ntrial = 10000 times for one experiment. 
+Now, think about a coin for which the probability of a head is 0.1%. You toss the coin Ntrial = 100000 times for one experiment. 
 If you repeat the experiment 1000 times, what does the distribution look like? Take the mean and standard deviations of the 1000 Nhead values. 
 Do they match the predicted mean and standard deviation of the Poisson distribution?
 """
@@ -10,7 +10,7 @@ import numpy as np
 
 N = 200
 sampleSpace = ['H','T']
-t = 100
+t = 100000
 heads = []
 
 for j in range(t):
@@ -27,6 +27,6 @@ mean = np.mean(heads)
 sd = np.std(heads)
 print("the mean is:",mean, "and the standard deviation is",sd)
 
-trueMean = N*0.5
-trueSD = np.sqrt(N*0.5*0.5)
+trueMean = N*0.1
+trueSD = np.sqrt(N*0.1*0.9)
 print("the mean should be:",trueMean, "and the standard deviation should be",trueSD)
